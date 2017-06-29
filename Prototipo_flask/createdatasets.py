fd982ab61df6c726628a9471a167afcaeae0df2a
mrr0088 <mrr0088@alu.ubu.es>
2017-06-29 20:42:45 +0200

    Update createdatasets.py

--- Prototipo_flask/createdatasets.py
+++ Prototipo_flask/createdatasets.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 cliente = MongoClient()
 db = cliente.test_database
 datasets = db.datasets
-datasets.insert_one({'dataset':'Machismo','tag':{'clase1':'aFavor','clase2':'enContra'}})
-datasets.insert_one({'dataset':'VientreAlquiler','tag':{'clase1':'aFavor','clase2':'enContra'}})
+datasets.insert_one({'dataset':'Machismo','clases':{'clase1':'aFavor','clase2':'enContra'}})
+datasets.insert_one({'dataset':'VientreAlquiler','clases':{'clase1':'aFavor','clase2':'enContra'}})
 
 print 'datasets iniciales incluidos'
